b2c211f88e6777a81afee19cc8b75df2d2f13333
Joam Delgado <jondel08@gmail.com>
2022-10-18 09:38:16 -0500

    feat: adding images an routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Series from './pages/Series';
+import Comics from './pages/Comic';
+import Error404 from './pages/Error404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element ={<Home/>} />
+          <Route path="/inicio" exact element={<Home/>} />
+          <Route path="/series" exact  element={<Series/>} />
+          <Route path="/comics" exact element={<Comics/>} />
+          <Route element={<Error404/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
